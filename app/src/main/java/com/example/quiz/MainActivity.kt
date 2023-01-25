dba35ccdd041df6c3517b5c8e00f41638e14362b
Mansur <mahcypc@mail.ru>
2023-01-25 22:45:04 +0300

    Added questions and Next button

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.quiz
 
-import android.app.ProgressDialog.show
 import android.os.Bundle
-import android.view.Gravity
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,12 +10,40 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+    private var currentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fun updateQuestion() {
+            val questionTextResId = questionBank[currentIndex].textResId
+            questionTextView.setText(questionTextResId)
+        }
+        fun checkAnswer(userAnswer: Boolean){
+            val correctAnswer = questionBank[currentIndex].answer
+            val messageResId =
+                if (userAnswer == correctAnswer) R.string.correct_toast
+                else R.string.incorrect_toast
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+        }
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
-        trueButton.setOnClickListener { Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()}
-        falseButton.setOnClickListener { Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show() }
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+        trueButton.setOnClickListener { checkAnswer(true)}
+        falseButton.setOnClickListener { checkAnswer(false)}
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+         updateQuestion()
     }
 }
